0e4f463d567d562a071633e5d9ed4e277e0321f0
rosercoder <rosercoder@gmail>
2022-06-15 11:27:23 +0800

    (test) Create test class

---
+++ src/test/java/cool/wangshuo/ts/TsApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.wangshuo.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class TsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}

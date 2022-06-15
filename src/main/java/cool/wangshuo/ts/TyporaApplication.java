77b0935cc19307c061a483410c1c9982ddb7d0cf
rosercoder <rosercoder@gmail>
2022-06-15 11:27:51 +0800

    (style) Delete useless code

--- src/main/java/cool/wangshuo/ts/TyporaApplication.java
+++ src/main/java/cool/wangshuo/ts/TyporaApplication.java
@@ -12,7 +12,6 @@ public class TyporaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TyporaApplication.class, args);
-        System.out.println("http://127.0.0.1:8093");
     }
 
 }
